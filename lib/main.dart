11ecc5928f1ba32a90339ef826e46ad5d1cfd8b8
sho_otb <shota715otobe@gmail.com>
2023-02-13 11:41:11 +0900

    “post_display”

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_twitter/view/time_line/time_line_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TimeLinePage(),
+    );
+  }
+}
